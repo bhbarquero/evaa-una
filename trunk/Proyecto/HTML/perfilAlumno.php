128df75fddb01c7e93789bcf9dbbf55ff8bf3e3e
Peter17iobando <Peter17iobando@8e27bb4d-69fb-a78c-8d3a-0000004e441d>
2014-10-19 03:42:39 +0000

    js de funcion de ajax para llamar php con funcion de registrarProfesor

---
+++ trunk/Proyecto/HTML/perfilAlumno.php
@@ -0,0 +1,64 @@
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+
+<script src="../Scripts/jquery-2.1.1.min.js"></script>
+<script src="../Scripts/perfilalumno.js" type="text/javascript"></script>
+
+</head>
+
+<body>
+<header class="encabezado">Evaa
+</header>
+<div id="">
+  <form method="post" name="form1" >
+    <table align="center">
+      <tr valign="baseline">
+        <td nowrap align="right">Cedula:</td>
+        <td><input type="text" id="Cedula" value="" size="32" required></td>
+      </tr>
+      <tr valign="baseline">
+        <td nowrap align="right">Nombre:</td>
+        <td><input type="text" id="Nombre" value="" size="32" required></td>
+      </tr>
+      <tr valign="baseline">
+        <td nowrap align="right">Apellido:</td>
+        <td><input type="text" id="Apellido" value="" size="32" required></td>
+      </tr>
+      <tr valign="baseline">
+        <td nowrap align="right">FechaNacimiento:</td>
+        <td><input type="date" id="FechaNacimiento" value="" size="32" required></td>
+      </tr>
+      <tr valign="baseline">
+        <td nowrap align="right">Direccion:</td>
+        <td><input type="text" id="Direccion" value="" size="32" required></td>
+      </tr>
+      <tr valign="baseline">
+        <td nowrap align="right">Telefono Fijo:</td>
+        <td><input type="text" id="TelefonoFijo" value="" size="32" required></td>
+      </tr>
+      <tr valign="baseline">
+        <td nowrap align="right">Telefono Movil:</td>
+        <td><input type="text" id="TelefonoMovil" value="" size="32" required></td>
+      </tr>
+      <tr valign="baseline">
+        <td nowrap align="right">&nbsp;</td>
+        <td><input id="btnInsertarAlumno" type="submit" value="Insertar registro" ></td>
+      </tr>
+    </table>
+    <input type="hidden" name="MM_insert" value="form1">
+  </form>
+  <p>&nbsp;</p>
+  
+  
+  
+</div>
+
+
+<footer>
+</footer>
+</body>
+</html>
