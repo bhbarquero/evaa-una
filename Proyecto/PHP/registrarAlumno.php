6ec63446c374cccd32fcb2f9dbe56a3b2c94c952
Peter17iobando <Peter17iobando@8e27bb4d-69fb-a78c-8d3a-0000004e441d>
2014-10-19 04:29:36 +0000

    Eliminacion del index, Se crea php de registrar alumno (guarda datos de persona y de estudiante)

---
+++ Proyecto/PHP/registrarAlumno.php
@@ -0,0 +1,75 @@
+<?php
+
+//REGISTRAR la dirección
+
+if(isset($_POST['cedula']))
+// && isset($_POST['pass']) && isset($_POST['activo']) && isset($_POST['vence']) && isset($_POST['tiempo']))
+{
+	$conexion=mysqli_connect("localhost","root","","evaa_bd");
+	//COMPROBAR SI HUBO UN ERROR EN LA CONEXION
+	if(mysqli_connect_errno())
+	{
+		echo "Error al conectar con la BD. ".mysqli_connect_error();
+	}
+	else
+	{
+		$Cedula=$_POST['cedula'];		
+		$Nombre=$_POST['nombre'];
+		$Apellido=$_POST['apellido'];
+		$FechaNacimiento=$_POST['fechanacimiento'];
+		$Direccion=$_POST['direccion'];
+		$TelefonoFijo=$_POST['telefonofijo'];
+		$TelefonoMovil=$_POST['telefonomovil'];
+		$FechaIngreso=$_POST['fechaingreso'];
+		
+		$consulta="insert into tb_persona
+			(
+				Cedula,
+				Nombre,
+				Apellido,
+				FechaNacimiento,
+				Direccion,
+				TelefonoFijo,
+				TelefonoMovil		
+			)
+			values
+			(
+				'$Cedula',
+				'$Nombre',
+				'$Apellido',
+				'$FechaNacimiento',
+				'$Direccion',
+				'$TelefonoFijo',
+				'$TelefonoMovil'
+							
+			);
+		";
+		
+		$consulta1="insert into tb_estudiante
+			(
+				Cedula,
+				FechaIngreso
+			)
+			values
+			(
+				'$Cedula',
+				'$FechaIngreso'			
+			);
+			";
+		
+	if($resultado=mysqli_query($conexion,$consulta)&&$resultado1=mysqli_query($conexion,$consulta1))
+		{
+			echo "Guardado con exito";
+		}
+		else
+		{
+			echo "Error al acortar la dirección: ".mysqli_connect_error();
+		}
+		
+	}
+	//cerrar la conexion
+	mysqli_close($conexion);
+}
+else
+{echo "Faltan Datos Requeridos";}
+?>
