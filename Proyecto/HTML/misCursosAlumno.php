16bcc8dcc34577cc0ef7ee192bc742d28cf3a4ec
rvalverde182 <rvalverde182@8e27bb4d-69fb-a78c-8d3a-0000004e441d>
2014-10-19 08:01:56 +0000

    Listar Cursos Estudiantes, filtrando por nombre

---
+++ Proyecto/HTML/misCursosAlumno.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+
+<script src="../Scripts/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="../Scripts/misCursosAlumno.js"></script>
+
+</head>
+
+<body>
+	<div id="divTitulo">
+        <header>
+            <label>EVAA</label>
+            <h1>Mis Cursos</h1>
+            <label>Listado de Cursos de Estudiante </label>
+        </header>
+    </div>
+            
+    <div id="divContenido">
+    	<br><label><strong>Filtrar Cursos</strong></label>
+    	<br><input type="text" id="NomCurso" placeholder="Nombre Curso"> &nbsp; <input type="text" name="Año" placeholder="Año"><br>
+
+		<div id="divTablaResultados">
+        
+        </div>
+
+    </div>
+    <footer>
+    </footer>
+</body>
+</html>
---
+++ Proyecto/PHP/listarCursosAlumno.php
@@ -0,0 +1,20 @@
+		<?php 
+        $link = mysql_connect("localhost", "root"); 
+        mysql_select_db("evaa_bd", $link); 
+        
+        //Listar Cursos Estudiantes, filtrando por CORREO
+		$consulta = "SELECT Descripcion FROM tb_curso, tb_grupo, tb_matricula 
+                                WHERE tb_curso.CursoId = tb_grupo.CursoId 
+                                AND tb_grupo.GrupoId = tb_matricula.GrupoId 
+								AND tb_curso.Descripcion LIKE '%".$_POST['Nombre']."%'
+                                AND tb_matricula.usuarioCoreo = '123@gmail.com'";			
+								
+		if (mysql_num_rows( $result = mysql_query($consulta, $link))>0){ 
+			$tabla ="<table border = '1'><br/><tr><td><b>Nombre Cursos</b></td></tr> \n";
+				while($row = mysql_fetch_row($result))   
+					$tabla= $tabla."<tr><td>".$row[0]."</td></tr> \n </table> \n";
+				echo $tabla; 
+		}else 
+			echo "<br>No se ha encontrado ningún curso!";	
+		mysql_close($link);
+        ?> 
