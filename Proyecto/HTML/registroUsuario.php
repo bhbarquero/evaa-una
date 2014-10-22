211c83e206e40af7bd8f0075b6e568836584face
bhbarquero <bhbarquero@8e27bb4d-69fb-a78c-8d3a-0000004e441d>
2014-10-22 06:05:29 +0000

    master pages (css, js)

--- Proyecto/HTML/registroUsuario.php
+++ Proyecto/HTML/registroUsuario.php
@@ -3,42 +3,45 @@
 <head>
 <meta charset="utf-8">
 <title>Registro de usuario</title>
-
 <link type="text/css" rel="stylesheet" href="../Styles/General.css">
+<link type="text/css" rel="stylesheet" href="../Styles/forms.css">
+<link type="text/css" rel="stylesheet" href="../Styles/buttons.css">
 
 <script src="../Scripts/jquery-2.1.1.min.js"></script>
 <script src="../Scripts/registroUsuario.js" type="text/javascript"></script>
-
+<script src="../Scripts/General.js" type="text/javascript"></script>
 </head>
 
 <body>
-<header class="encabezado">
-<h1>EVAA</h1>
-</header>
-<div>
-<form id="form1">
-  <table align="center">
-    <tr valign="baseline">
-      <td nowrap align="right">Correo electrónico:</td>
-      <td><input type="text" id="CorreoUsuario" value="" size="32" required></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap align="right">Contraseña:</td>
-      <td><input type="text" id="Contrasena" value="" size="32" required></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap align="right">Tipo de Usuario:</td>
-      <td><select id="TipoUsuarioId">
-        <option value="1">Profesor</option>
-        <option value="2">Estudiante</option>
-      </select></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap align="right">&nbsp;</td>
-      <td><a id="btnRegistrar2">Insertar registro </a></td>
-    </tr>
-  </table>
-</form>
+<div id="layout"> <a href="#notifications" id="notificationsLink" class="notifications-link"> 
+  <!-- Hamburger icon --> 
+  <span></span> l</a>
+  <div id="notifications">
+    <div > <a href="#"></a> </div>
+  </div>
+  <div id="main">
+    <header>
+      <h1>EVAA</h1>
+    </header>
+    <div class="content">
+      <h2 class="content-subhead">Crea tu cuenta</h2>
+      <form id="frmResgistro" class="pure-form pure-form-stacked">
+        <label for="CorreoUsuario">Correo electrónico:</label>
+        <input type="email" id="CorreoUsuario" value="" size="32" placeholder="Obligatorio" required>
+        <label for="Contrasena">Contraseña:</label>
+        <input type="password" id="Contrasena" value="" size="32" required>
+        <label for="ReContrasena">Contraseña:</label>
+        <input type="password" id="ReContrasena" value="" size="32" required>
+        <label for="TipousuarioId" >Tipo de Usuario:</label>
+        <select id="TipoUsuarioId">
+          <option value="1">Profesor</option>
+          <option value="2">Estudiante</option>
+        </select>
+        <br/>
+        <a id="btnRegistrar2" class="pure-button pure-button-primary" >Registrarse </a>
+      </form>
+    </div>
+  </div>
 </div>
 </body>
 </html>
