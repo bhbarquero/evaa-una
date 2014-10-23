66fa66c205d8e58cd1a32a30b935c8c1625b66dd
bhbarquero <bhbarquero@8e27bb4d-69fb-a78c-8d3a-0000004e441d>
2014-10-23 05:19:59 +0000

    Agrego una plantilla del master pages funcional.

---
+++ trunk/Proyecto/Scripts/Master.js
@@ -0,0 +1,36 @@
+(function (window, document) {
+
+    var layout   = document.getElementById('layout'),
+        notif     = document.getElementById('notif'),
+        notifLink = document.getElementById('notifLink');
+
+    function toggleClass(element, className) {
+        var classes = element.className.split(/\s+/),
+            length = classes.length,
+            i = 0;
+
+        for(; i < length; i++) {
+          if (classes[i] === className) {
+            classes.splice(i, 1);
+            break;
+          }
+        }
+         //The className is not found
+        if (length === classes.length) {
+            classes.push(className);
+        }
+
+        element.className = classes.join(' ');
+    }
+
+    notifLink.onclick = function (e) {
+        var active = 'active';
+
+        e.preventDefault();
+        toggleClass(layout, active);
+        toggleClass(notifications, active);
+        toggleClass(notificationsLink, active);
+    };
+
+}(this, this.document));
+
