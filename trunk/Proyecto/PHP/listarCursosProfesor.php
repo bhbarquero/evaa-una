2b93c7da9edc4736e887957ca3a114f1fa3940ea
bhbarquero <bhbarquero@8e27bb4d-69fb-a78c-8d3a-0000004e441d>
2014-11-09 05:02:30 +0000

    lista sesion en listados de cursos

--- trunk/Proyecto/PHP/listarCursosProfesor.php
+++ trunk/Proyecto/PHP/listarCursosProfesor.php
@@ -1,14 +1,16 @@
 		<?php 
         $link = mysql_connect("localhost", "root"); 
         mysql_select_db("evaa_bd", $link); 
-
+		session_start();
+		
         //Listar Cursos de cada Profesor
 		$consulta = "SELECT Descripcion, (tb_grupo.Anno) as Anno, (tb_grupo.GrupoId) as GrupoId
-							 FROM tb_curso, tb_grupo, tb_profesor 
+							 FROM tb_curso, tb_grupo, tb_profesor, tb_persona
                                 WHERE tb_curso.CursoId = tb_grupo.CursoId 
                                 AND tb_grupo.Pofesor = tb_profesor.Cedula 
 								AND tb_curso.Descripcion LIKE '%".$_POST['Nombre']."%'
-                                AND tb_profesor.Cedula  = '123456'";			
+								AND tb_persona.CorreoUsuario = '". $_SESSION["user"]."'
+								AND tb_persona.Cedula = tb_profesor.Cedula";			
 								
 		if ($_POST['AnnoGrupo'] != "")
 			$consulta .= " AND tb_grupo.Anno = ".$_POST['AnnoGrupo']."";
