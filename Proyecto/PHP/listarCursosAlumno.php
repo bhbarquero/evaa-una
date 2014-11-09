69cccb7796db87c13bd97b011ecaea7042f51e09
bhbarquero <bhbarquero@8e27bb4d-69fb-a78c-8d3a-0000004e441d>
2014-11-09 05:02:30 +0000

    lista sesion en listados de cursos

--- Proyecto/PHP/listarCursosAlumno.php
+++ Proyecto/PHP/listarCursosAlumno.php
@@ -2,18 +2,17 @@
         $link = mysql_connect("localhost", "root"); 
         mysql_select_db("evaa_bd", $link); 
 		session_start();
-		$sesi = $_SESSION["user"];
 		
         //Listar Cursos Estudiantes, filtrando por CORREO
 		$consulta = "SELECT Descripcion FROM tb_curso, tb_grupo, tb_matricula 
                                 WHERE tb_curso.CursoId = tb_grupo.CursoId 
                                 AND tb_grupo.GrupoId = tb_matricula.GrupoId 
 								AND tb_curso.Descripcion LIKE '%".$_POST['Nombre']."%'
-                                AND tb_matricula.CorreoUsuario  = ". $sesi;			
+                                AND tb_matricula.CorreoUsuario  = '". $_SESSION["user"]."'";			
 								
 		if ($_POST['AnnoGrupo'] != "")
 			$consulta .= " AND tb_grupo.Anno = ".$_POST['AnnoGrupo']."";
-		
+echo $consulta;
 		if ($result = mysql_query($consulta, $link)) {	
 			if (mysql_num_rows($result = mysql_query($consulta, $link))>0){ 
 				$tabla ="<table border = '1'><br/><tr><td><b>Historial de Cursos</b></td></tr> \n";
