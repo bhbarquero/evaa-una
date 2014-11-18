dbe7cb33464e83f990c56a22ed52d960daf86f0d
carlostenoriosolis@gmail.com <carlostenoriosolis@gmail.com@8e27bb4d-69fb-a78c-8d3a-0000004e441d>
2014-11-18 22:27:01 +0000

    final tenorio

--- trunk/Proyecto/Scripts/calificarNota.js
+++ trunk/Proyecto/Scripts/calificarNota.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 function funSeleccionar(correo) {
 	document.getElementById('cedula').value=correo;
-	document.getElementById('btnBoton').style.display='block';
+	document.getElementById('diAcciones').style.display='block';
 	document.getElementById('txtNota').style.display='block';
 	
 }
@@ -19,7 +19,7 @@ $(document).ready(function(e) {
 			type: "POST",
 
 			success: function(response){
-				$('#divResultado').html(response);
+				$('#divTablaResultados').html(response);
 			},
 			error: function(response){		
 				$.mensajeError("Error desconocido",4);
@@ -41,11 +41,15 @@ $(document).ready(function(e) {
 			dataType: 'json',
 
 			success: function(response){
-				alert(response.Mensaje)
-				document.location.reload(true);
+				if(response.TipoMensaje==1){
+				$.mensajeExito(response.Mensaje,4);
+				setTimeout(function(){document.location.reload(true);},4000);
+				}
+				else{
+				$.mensajeError("Error desconocido" +response.Mensaje,4);}
 			},
 			error: function(response){		
-				$.mensajeError("Error desconocido",4);
+				$.mensajeError("Error desconocido"+response.Mensaje,4);
 				}
 		});
     });
@@ -61,3 +65,21 @@ function funSeleccionar(a) {
 	
 	
 });
+
+$.mensajeExito = function(mensaje, segundos){
+		$(".msgContent").toggleClass("activeExito");
+		$("#mensaje").html(mensaje);
+		  
+		setTimeout(function(){
+			$(".msgContent").removeClass("activeExito");
+		},segundos*1000);
+	};
+
+$.mensajeError = function(mensaje, segundos){
+		$(".msgContent").toggleClass("activeError");
+		$("#mensaje").html(mensaje);
+		
+		setTimeout(function(){
+			$(".msgContent").removeClass("activeError");
+		},segundos*1000);
+	};
